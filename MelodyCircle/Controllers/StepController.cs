895be73de974cf55b43ecd1c7808123d16742f38
Rodrigo Nogueira <rodrigo.a.nogueira@gmail.com>
2024-03-13 15:29:58 +0000

    Criação de Passos em Tutoriais

--- MelodyCircle/Controllers/StepController.cs
+++ MelodyCircle/Controllers/StepController.cs
@@ -10,7 +10,6 @@ namespace MelodyCircle.Controllers
     public class StepController : Controller
     {
         private readonly ApplicationDbContext _context;
-        private Guid _tutorialId;
 
         public StepController(ApplicationDbContext context)
         {
@@ -23,12 +22,12 @@ namespace MelodyCircle.Controllers
             if (tutorialId == null)
                 return NotFound();
 
-            _tutorialId = tutorialId.Value;
-
             var steps = await _context.Steps
                 .Include(s => s.Tutorial)
-                .Where(s => s.TutorialId == _tutorialId)
+                .Where(s => s.TutorialId == tutorialId)
                 .ToListAsync();
+
+            ViewBag.TutorialId = tutorialId;
 
             return View(steps);
         }
@@ -36,9 +35,11 @@ namespace MelodyCircle.Controllers
         // GET: Step/Create
         public IActionResult Create(Guid? tutorialId)
         {
-            Console.WriteLine(_tutorialId);
-
             ViewBag.TutorialId = tutorialId;
+
+            if (tutorialId == null)
+                return NotFound();
+
             return View();
         }
 
@@ -47,72 +48,16 @@ namespace MelodyCircle.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("TutorialId,Title,Content,Order")] Step step)
         {
+            if (string.IsNullOrEmpty(step.Title) || string.IsNullOrEmpty(step.Content))
+                ModelState.AddModelError(nameof(step.Title), "O título é obrigatório");
+
+            if (string.IsNullOrEmpty(step.Content) || string.IsNullOrEmpty(step.Title))
+                ModelState.AddModelError(nameof(step.Title), "O conteúdo é obrigatório");
+
             _context.Add(step);
             await _context.SaveChangesAsync();
 
             return RedirectToAction("Index", new { tutorialId = step.TutorialId });
-        }
-
-        // GET: Step/Edit/id
-        public async Task<IActionResult> Edit(Guid? tutorialId)
-        {
-            if (tutorialId == null)
-                return NotFound();
-
-            var step = await _context.Steps.FindAsync(tutorialId);
-
-            if (step == null)
-                return NotFound();
-
-            return View(step);
-        }
-
-        // POST: Step/Edit/id
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Guid tutorialId, [Bind("Id,TutorialId,Title,Content,Order")] Step step)
-        {
-            if (tutorialId != step.Id)
-                return NotFound();
-
-            try
-            {
-                _context.Update(step);
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!StepExists(step.Id))
-                    return NotFound();
-                else
-                    throw;
-            }
-            return RedirectToAction("Details", "Tutorial", new { tutorialId = step.TutorialId });
-        }
-
-        // GET: Step/Delete/id
-        public async Task<IActionResult> Delete(Guid? tutorialId)
-        {
-            if (tutorialId == null)
-                return NotFound();
-
-            var step = await _context.Steps.FirstOrDefaultAsync(m => m.Id == tutorialId);
-
-            if (step == null)
-                return NotFound();
-
-            return View(step);
-        }
-
-        // POST: Step/Delete/id
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(Guid tutorialId)
-        {
-            var step = await _context.Steps.FindAsync(tutorialId);
-            _context.Steps.Remove(step);
-            await _context.SaveChangesAsync();
-            return RedirectToAction("Details", "Tutorial", new { tutorialId = step.TutorialId });
         }
 
         private bool StepExists(Guid tutorialId)
