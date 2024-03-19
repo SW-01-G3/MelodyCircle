a27416bf372635a51045982a803f0db1b4b48471
Guilherme-Bernardino <202001870@estudantes.ips.pt>
2024-03-19 20:33:36 +0000

    116 + fixes
    Add music spotify
    Profile edit buttons

---
+++ MelodyCircle/ViewModels/UserMusicsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MelodyCircle.Models;
+
+namespace MelodyCircle.ViewModels
+{
+    public class UserMusicsViewModel
+    {
+        public User User { get; set; }
+
+        public IList<string> MusicUri { get; set; }
+    }
+}
