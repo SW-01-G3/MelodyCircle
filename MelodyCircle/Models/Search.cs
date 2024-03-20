7199c1a3e27f03e885ec0a67a7b13819a47bf870
Rodrigo Nogueira <rodrigo.a.nogueira@gmail.com>
2024-03-20 23:53:01 +0000

    Pesquisa e filtros feitos

--- MelodyCircle/Models/Search.cs
+++ MelodyCircle/Models/Search.cs
@@ -4,11 +4,12 @@ namespace MelodyCircle.Models
     public class Search
     {
         public string SearchTerm { get; set; }
-        public SearchType SearchType { get; set; }
+        public SearchType? SearchType { get; set; }
     }
 
     public enum SearchType
     {
+        None,
         User,
         Tutorial,
         Collaboration
