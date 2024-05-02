2563825d99f0e0b4011ac0288ce74c599a4c8acd
Guilherme-Bernardino <202001870@estudantes.ips.pt>
2024-05-02 13:24:26 +0100

    migrations

--- MelodyCircle/Migrations/20240502122345_initial.Designer.cs
+++ MelodyCircle/Migrations/20240502122345_initial.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MelodyCircle.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20240413193143_initial")]
+    [Migration("20240502122345_initial")]
     partial class initial
     {
         /// <inheritdoc />
